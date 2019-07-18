7e0afc5847ef2687ce0c12f93e9d6e2d63af2cc0
Javad <javad.noorbakhsh@gmail.com>
2019-07-18 10:12:04 -0400

    add minus operator

--- calc.py
+++ calc.py
@@ -1,11 +1,12 @@
 def compute(string):
     values = string.split(' ')
     num0 = int(values[0])
-    num1 = int(values[0])
+    num1 = int(values[2])
     operator = values[1]
     if operator == '+':
         return num0 + num1
+    elif operator == '-':
+        return num0 - num1
     else:
-        print('Unknown operator!')
-        return 0
+        raise ValueError('Unknown operator!')
 
