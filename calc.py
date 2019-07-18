84770d5ca88040a92e0ab7cb4c84f9ab04256f67
Javad <javad.noorbakhsh@gmail.com>
2019-07-18 10:43:41 -0400

    docstring

--- calc.py
+++ calc.py
@@ -1,4 +1,9 @@
 def compute(string):
+    """
+    Perform simple arithmetic based on string
+    
+    Example: '5 + 7' -> 12
+    """
     values = string.split(' ')
     num0 = int(values[0])
     num1 = int(values[2])
