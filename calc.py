80bd40d748b49d041cee6a8c325542ce50fa4701
Javad <javad.noorbakhsh@gmail.com>
2019-07-18 10:06:27 -0400

    first file

---
+++ calc.py
@@ -0,0 +1,11 @@
+def compute(string):
+    values = string.split(' ')
+    num0 = int(values[0])
+    num1 = int(values[0])
+    operator = values[1]
+    if operator == '+':
+        return num0 + num1
+    else:
+        print('Unknown operator!')
+        return 0
+
