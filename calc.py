0722dcfe9d3e603bad1b9a3fd4716ef5aee7a61b
Martine Seignon <martine.seignon@jax.org>
2019-07-18 11:41:50 -0400

    add support for float

--- calc.py
+++ calc.py
@@ -5,8 +5,8 @@ def compute(string):
     Example: '5 + 7' -> 12
     """
     values = string.split(' ')
-    num0 = int(values[0])
-    num1 = int(values[2])
+    num0 = float(values[0])
+    num1 = float(values[2])
     operator = values[1]
     if operator == '+':
         return num0 + num1
